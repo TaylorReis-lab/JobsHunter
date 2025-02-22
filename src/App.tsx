0948e24ae81f7401f870f03480596511bf8aa461
Taylor Reis <reist98822@gmail.com>
2025-02-22 13:41:18 -0300

    create home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from 'react'
+import Home from './containers/home/home'
 
 function App() {
-  return (
-    <div>Test</div>
-  );
+  return <Home />
 }
 
-export default App;
+export default App
